13d31c0fb79b3fd8f069b97cc51029e3c977fdd2
Jim <jim@advancebass.com>
2020-09-02 07:26:30 -0700

    Updates

---
+++ precache-manifest.b61f0080d6d23ff96c0c4ce95aaac734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc598c71f1256be719d4409582d6644f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3deaff37d2be083ec75",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aafc0f50f64bd3a31b99",
+    "url": "/static/js/2.dab83196.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab83196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3deaff37d2be083ec75",
+    "url": "/static/js/main.277aa827.chunk.js"
+  },
+  {
+    "revision": "76dd4ec5074e929d5051",
+    "url": "/static/js/runtime-main.dbacfaea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
